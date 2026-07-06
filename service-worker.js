13735081133564336c506b4625f9e1f8c670f4c3
collector-taskhub <jalnacollectorpa@gmail.com>
2026-07-06 18:04:21 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = 'sir-monitor-v2';
+const SHELL = ['./index.html', './app.js', './styles.css', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+});
