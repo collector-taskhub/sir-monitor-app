1cf9fc495583292a225eb6c3cd1205b44c7cef53
collector-taskhub <jalnacollectorpa@gmail.com>
2026-07-07 10:24:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,19 @@
-const CACHE = 'sir-monitor-v3';
+const CACHE = 'sir-monitor-v4'; // bump this EVERY time you change app.js/index.html/styles.css
 const SHELL = ['./index.html', './app.js', './styles.css', './manifest.json'];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // activate the new worker immediately, don't wait for old tabs to close
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)) // wipe old caches
+      )
+    ).then(() => self.clients.claim()) // take control of open tabs right away
+  );
 });
 
 self.addEventListener('fetch', (e) => {
